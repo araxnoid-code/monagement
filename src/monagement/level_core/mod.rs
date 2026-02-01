78e4856099e0dc9dc5d446c1e347fcd5b01855e9
araxnoid <araxnoid0@gmail.com>
2026-02-01 17:46:44 +0700

    decided to use linked list on second level link

--- src/monagement/level_core/mod.rs
+++ src/monagement/level_core/mod.rs
@@ -1,8 +1,17 @@
+use crate::Node;
+
 #[derive(Clone, Debug)]
 pub struct FirstLevel {
     pub(crate) count: u64,
     pub(crate) bitmap: u64,
     pub(crate) sl_list: Vec<SecondLevel>,
+}
+
+pub struct SecondLayerLink {
+    index: usize,
+    node: Node,
+    front: Option<usize>,
+    back: Option<usize>,
 }
 
 #[derive(Clone, Debug)]
